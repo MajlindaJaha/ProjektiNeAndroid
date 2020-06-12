3e20cdfac9af86065672e772c1a4301403b824ac
GentianVeliu <44606115+GentianVeliu@users.noreply.github.com>
2020-06-12 23:55:06 +0200

    Krijimi i vektorit 2D(matrices) te butonave ku i caktojme ne menyre dinamike me ane te findViewByID dhe OnClickListener mbi ta duke e perdorur llupen nested for. Do te krijojme nje metode qe kontrollon per fitues duke kaluar ne te gjitha rreshtat, kolonat dhe diagonalet e matrices per te pare nese njeri prej tyre ka 3 fushat qe perputhen..

--- app/src/main/java/com/fiek/projektineandroid/Loja.java
+++ app/src/main/java/com/fiek/projektineandroid/Loja.java
@@ -3,12 +3,128 @@ package com.fiek.projektineandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class Loja extends AppCompatActivity {
+public class Loja extends AppCompatActivity implements View.OnClickListener{
+
+    private Button[][] buttons = new Button[3][3];
+
+    private boolean lojtari1Radhen = true;
+
+    private int numeroRundat;
+
+    private int lojtari1Piket;
+    private int lojtari2Piket;
+
+    private TextView textViewLojtari1;
+    private TextView textViewLojtari2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loja);
+
+        textViewLojtari1 = findViewById(R.id.text_view_l1);
+        textViewLojtari2 = findViewById(R.id.text_view_l2);
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                String buttonId = "btn_" + i + j;
+                int resID = getResources().getIdentifier(buttonId, "id", getPackageName());
+                buttons[i][j] = findViewById(resID);
+                buttons[i][j].setOnClickListener(this);
+
+            }
+        }
+
+        Button buttonReset = findViewById(R.id.btn_reseto);
+        buttonReset.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
+
+    @Override
+    public void onClick(View v) {
+        if(!((Button) v).getText().toString().equals("")){
+            return;
+        }
+
+        if (lojtari1Radhen)
+        {
+            ((Button) v).setText("X");
+        }
+        else
+            {
+                ((Button) v).setText("O");
+            }
+
+        numeroRundat++;
+
+        if (kontrolloPerFitues()) {
+            if (lojtari1Radhen)
+            {
+                lojtari1Fiton();
+            }
+            else {
+                lojtari2Fiton();
+            }
+        } else if (numeroRundat == 9) {
+            barazim();
+        } else {
+            lojtari1Radhen = !lojtari1Radhen;
+        }
+
+        }
+
+    private boolean kontrolloPerFitues()
+    {
+        String[][] field = new String[3][3];
+
+        for (int i = 0; i < 3; i++){
+            for (int j = 0; j < 3; j++){
+                field[i][j] = buttons[i][j].getText().toString();
+            }
+        }
+
+        for (int i = 0; i < 3; i++) {
+            if (field[i][0].equals(field[i][1])
+                    && field[i][0].equals(field[i][2])
+                    && !field[i][0].equals("")) {
+                return true;
+            }
+        }
+
+        for (int i = 0; i < 3; i++) {
+            if (field[0][i].equals(field[1][i])
+                    && field[0][i].equals(field[2][i])
+                    && !field[0][i].equals("")) {
+                return true;
+            }
+        }
+
+        if (field[0][0].equals(field[1][1])
+                && field[0][0].equals(field[2][2])
+                && !field[0][0].equals("")) {
+            return true;
+        }
+
+        if (field[0][2].equals(field[1][1])
+                && field[0][2].equals(field[2][0])
+                && !field[0][2].equals("")) {
+            return true;
+        }
+
+        return false;
+
+    }
+
+    private void lojtari1Fiton() {}
+    private void lojtari2Fiton() {}
+    private void barazim() {}
+
 }
