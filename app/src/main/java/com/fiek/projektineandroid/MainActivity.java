5cd9cc6a5604aede5952072247a8eec04f9fc5ea
MajlindaJaha <44609607+MajlindaJaha@users.noreply.github.com>
2020-06-20 03:21:33 +0200

    Vazhdimi i Fragmenteve ne activity_main.xml dhe permiresimi i disa gabimeve

--- app/src/main/java/com/fiek/projektineandroid/MainActivity.java
+++ app/src/main/java/com/fiek/projektineandroid/MainActivity.java
@@ -1,14 +1,15 @@
 package com.fiek.projektineandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TableLayout;
+import com.google.android.material.tabs.TabLayout;
 
-public class MainActivity extends AppCompatActivity {
-    private Button button;
+public class MainActivity extends AppCompatActivity{
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,18 +17,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        button = (Button) findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                openRegjistrimi();
-            }
-        });
 
     }
 
-    public void openRegjistrimi(){
-        Intent intent = new Intent(this, Regjistrimi.class);
-        startActivity(intent);
-    }
+
 }
