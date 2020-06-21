d2a2a01bab60f90a2675fe6184c6955f5345e032
GentianVeliu <44606115+GentianVeliu@users.noreply.github.com>
2020-06-22 01:37:19 +0200

    Rregullimi i fragmenteve (Permbushja e kerkeses per krijimin e fragmenteve)

--- app/src/main/java/com/fiek/projektineandroid/MainActivity.java
+++ app/src/main/java/com/fiek/projektineandroid/MainActivity.java
@@ -1,14 +1,15 @@
 package com.fiek.projektineandroid;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import android.content.Intent;
+import androidx.fragment.app.Fragment;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TableLayout;
+import android.view.MenuItem;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.tabs.TabLayout;
 
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends AppCompatActivity {
 
 
     @Override
@@ -16,7 +17,32 @@ public class MainActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_nav);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
 
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_pamja,
+                new Kryefaqja()).commit();
     }
 
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                    Fragment selectedFragment = null;
+
+
+                    switch (menuItem.getItemId()) {
+                        case R.id.nav_kryefaqja:
+                            selectedFragment = new Kryefaqja();
+                            break;
+                        case R.id.nav_histori:
+                            selectedFragment = new Historiku();
+                            break;
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_pamja,
+                            selectedFragment).commit();
+
+                    return true;
+                }
+            };
 }
